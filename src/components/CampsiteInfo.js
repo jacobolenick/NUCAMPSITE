727e86052db399f7bf15fbb4eb8416fafed4a63f
jacobolenick <jacobolenick@Jacobs-MacBook-Pro.local>
2021-02-13 18:00:54 -0800

    Updated model on campsite info page

--- src/components/CampsiteInfo.js
+++ src/components/CampsiteInfo.js
@@ -1,16 +1,118 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Col, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from "react-redux-form";
+
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+
+class CommentForm extends Component {
+  state = { isModalOpen: false };
+
+  toggleModal = () => {
+    this.setState({ isModalOpen: !this.state.isModalOpen });
+  };
+
+  handleSubmit(values) {
+    this.toggleModal();
+    this.props.postComment(
+      this.props.campsiteId,
+      values.rating,
+      values.author,
+      values.text
+    );
+  }
+
+
+
+  render() {
+    return (
+
+      <div><Button outline onClick={this.toggleModal}><i className="fa fa-pencil fa-lg" /> Submit Comment </Button>
+        {/* add modal here */}
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>submit Comment</ModalHeader>
+          <ModalBody>
+          <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+              <Label htmlFor="rating" md={12}>
+                Rating
+              </Label>
+              <Col md={12}>
+                <Control.select
+                  model=".rating"
+                  id="rating"
+                  name="rating"
+                  placeholder="rating"
+                  className="form-control"
+                >
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </Control.select>
+              </Col>
+              <Label htmlFor="author" md={12}>
+                Author
+              </Label>
+              <Col>
+                <Control.text
+                  model=".author"
+                  id="author"
+                  name="author"
+                  placeholder="Your Name"
+                  className="form-control"
+                  validators={{
+                    minLength: minLength(2),
+                    maxLength: maxLength(15),
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".author"
+                  show="touched"
+                  component="div"
+                  messages={{
+                    required: "Required",
+                    minLength: "Must be at least 2 characters",
+                    maxLength: "Must be 15 characters or less",
+                  }}
+                />
+              </Col>
+              <Col>
+                <Label htmlFor="text">Comment</Label>
+                <Control.textarea
+                  model=".text"
+                  id="text"
+                  name="text"
+                  placeholder=""
+                  className="form-control"
+                  rows={6}
+                />
+              </Col>
+              <Col>
+                <Button type="submit" value="submit" color="primary">
+                  Submit Comment
+                </Button>
+              </Col>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </div>
+    )
+
+  };
+}
 
 
 function RenderCampsite({ campsite }) {
   return (
     <div className="col-md-5, m-1">
       <Card style={{ radius: "20px", shadow: "#000000" }}>
-      <CardImg top src={campsite.image} alt={campsite.name} />
-      <CardBody>
-        <CardText>{campsite.description}</CardText>
-      </CardBody>
+        <CardImg top src={campsite.image} alt={campsite.name} />
+        <CardBody>
+          <CardText>{campsite.description}</CardText>
+        </CardBody>
       </Card>
     </div >
   )
@@ -29,11 +131,17 @@ function RenderComments({ comments }) {
             </div>
           );
         })}
+
+        <CommentForm />
+
       </div>
     );
   }
   return <div />;
 }
+
+
+
 function CampsiteInfo(props) {
   if (props.campsite) {
     return (
